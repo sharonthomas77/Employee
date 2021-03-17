f6ce64cf638efc74ffde58d850106ca823e5abc9
sharonthomas77 <mca547@rajagiri.edu>
2021-03-17 15:33:16 +0530

    m

---
+++ resources/views/aboutus.php
@@ -0,0 +1,10 @@
+@extends("theme")
+@section("content")
+<div class="container">
+<div class="row">
+<tr>
+<td>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia fugit ipsa ratione, ducimus tempora aut consequatur cupiditate sed eligendi ad alias illo repellat explicabo provident nemo cum autem sunt. Omnis?</td>
+</tr>
+</div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/search', function () {
     return view('search');
 });
+
+Route::get('/aboutus', function () {
+    return view('aboutus');
+});
